4d8fb351811370d3c094e2568933a77d898d60a7
syedabdullah-alasoft <syedabdullah@alasoft.co>
2025-09-01 16:11:54 +0500

    add api2

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ app.get("/", (req, res) => {
 app.get("/health", (req, res) => {
   res.send("Everything is fine here");
 });
-app.get("/health2", (req, res) => {
-  res.send("Everything is fine here");
-});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
