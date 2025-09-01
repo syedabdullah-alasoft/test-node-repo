c10286fb9a3a0a791c998d31b9075768522f24f9
syedabdullah-alasoft <syedabdullah@alasoft.co>
2025-09-01 15:33:48 +0500

    test jenkins

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello from Node.js Express running via Jenkins & ECR!");
 });
+app.get("/health", (req, res) => {
+  res.send("Everything is fine here");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
