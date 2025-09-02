6eb74e819130a9ea25a8baa3c591fa1446b355a3
syedabdullah-alasoft <syedabdullah@alasoft.co>
2025-09-02 12:54:46 +0500

    add k8s files

--- index.js
+++ index.js
@@ -26,16 +26,4 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-// create ec2 -> Add IGW -> add SG
-// install jenkins -> enable -> start -> service
-// on port 8080 -> create user -> install pulgins docker, docker pipeline, aws cred(add creds) plugins
-// docker install -> add in jenkins group
-// aws cli -> aws configure -> add creds
-// create new pipeline with scm * * * * * -> add git repo -> also add webhook in github -> add pipeline code -> apply
-// Now add a docker file inside your project -> build and check -> push to github -> ad git repo where every needed
 
-    // aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 810119354065.dkr.ecr.us-east-1.amazonaws.com/
-    // docker pull  810119354065.dkr.ecr.us-east-1.amazonaws.com/test-jenkins-repo:6
-    // docker run -it -p 3000:3000 810119354065.dkr.ecr.us-east-1.amazonaws.com/test-jenkins-repo:6
-
-    
